07dcf959d5bf55a5b4ec60a4b377f40bab22efe3
Christopher Lane Hinson <lane@webperformance.com>
2017-05-09 15:26:10 -0400

    Adds random delay.

--- spec/io-delay-spec.js
+++ spec/io-delay-spec.js
@@ -2,20 +2,73 @@
 
 const delay = require('../src/index');
 const metrics = require('legion-metrics');
+const Io = require('legion-io');
 
 describe('The fetch module for legion Io', function() {
   it('is sane', function(done) {
-    delay(1).run(metrics.Target.create(metrics.merge).receiver()).then(done).catch(done.fail);
+    delay(1).run({services:{metrics:metrics.Target.create(metrics.merge).receiver()}}).then(done).catch(done.fail);
   });
 
   it('measures timings correctly', function(done) {
     const target = metrics.Target.create(metrics.merge);
 
-    delay(3).run(target.receiver()).then(function() {
+    delay(3).run({services:{metrics:target.receiver()}}).then(function() {
       const metrics = JSON.parse(JSON.stringify(target.get()));
 
       expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeGreaterThan(2995);
       expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeLessThan(3050);
+
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeGreaterThan(-5);
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeLessThan(50);
+
+      done();
+    }).catch(done.fail);
+  });
+
+  it('supports uniformly distributed delays', function(done) {
+    const target = metrics.Target.create(metrics.merge);
+    const tasks = [];
+
+    for( let i = 0; i < 1000; i++ )
+      tasks.push(delay(1,3));
+
+    Io.all(tasks).run({services:{metrics:target.receiver()}}).then(function() {
+      const metrics = JSON.parse(JSON.stringify(target.get()));
+
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeGreaterThan(900);
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeLessThan(1100);
+
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeGreaterThan(1750);
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeLessThan(2250);
+
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$max).toBeGreaterThan(2900);
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$max).toBeLessThan(3100);
+
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeGreaterThan(-5);
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeLessThan(50);
+
+      done();
+    }).catch(done.fail);
+  });
+
+  it('supports delays from a user-specified random number generator', function(done) {
+    const target = metrics.Target.create(metrics.merge);
+    const tasks = [];
+
+    for( let i = 0; i < 1000; i++ )
+      tasks.push(delay(() => Math.random() + Math.random() + 1));
+
+    Io.all(tasks).run({services:{metrics:target.receiver()}}).then(function() {
+      const metrics = JSON.parse(JSON.stringify(target.get()));
+
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeGreaterThan(900);
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeLessThan(1100);
+
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeGreaterThan(1750);
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeLessThan(2250);
+
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$max).toBeGreaterThan(2900);
+      expect(metrics.tags.protocol['delay'].values.delay_duration.$max).toBeLessThan(3100);
 
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeGreaterThan(-5);
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeLessThan(50);
--- src/index.js
+++ src/index.js
@@ -3,32 +3,71 @@
 const Io = require('legion-io');
 const metrics = require('legion-metrics');
 
-module.exports = function(seconds) {
-  if( typeof seconds !== 'number' )
-    throw new Error('delay: parameter must be a number in seconds');
+function validate(sample_points) {
+  for( let i = 0; i < sample_points.length; i++ ) {
+    if( !Number.isFinite(sample_points[i]) || sample_points[i] < 0 )
+      throw new Error('Not a valid sample point: ' + i);
+  }
+}
 
-  return Io.get()
-           .chain(receiver => {
-             const delay_start = Date.now();
-             return new Promise((resolve,reject) => {
-               try {
-                 setTimeout(resolve,seconds*1000);
-               } catch(e) {
-                 reject(e);
-               }
-             }).then(() => {
-               const delay_end = Date.now();
-               receiver.tag(metrics.tags.protocol('delay')).receive(metrics.sample({
-                 delay_start: metrics.sample.timestamp(delay_start),
-                 delay_end: metrics.sample.timestamp(delay_end),
-                 delay_duration: metrics.sample.duration(delay_end - delay_start),
-                 delay_discrepancy: {
-                   value: (delay_end - delay_start) - seconds*1000,
-                   unit: 'milliseconds',
-                   interpretation: 'Difference between the expected and actual delay as implemented by the setTimeout function.'
-                 }
-               }));
-             });
-           });
+module.exports = function() {
+  const f = module.exports.smart.apply(undefined,arguments);
+
+  return Io.get().chain(state => {
+    const delay_start = Date.now();
+    const milliseconds = f(delay_start)*1000; // *1000 to convert seconds to milliseconds
+    return new Promise((resolve,reject) => {
+      try {
+        setTimeout(resolve,milliseconds);
+      } catch(e) {
+        reject(e);
+      }
+    }).then(() => {
+      const delay_end = Date.now();
+      state.services.metrics.tag(metrics.tags.protocol('delay')).receive(metrics.sample({
+        delay_start: metrics.sample.timestamp(delay_start),
+        delay_end: metrics.sample.timestamp(delay_end),
+        delay_duration: metrics.sample.duration(delay_end - delay_start),
+        delay_intended: {
+          value: milliseconds,
+          unit: 'milliseconds',
+          interpretation: 'Delay length randomly selected from a statistical distribution.'
+        },
+        delay_discrepancy: {
+          value: (delay_end - delay_start) - milliseconds,
+          unit: 'milliseconds',
+          interpretation: 'Difference between the expected and actual delay as implemented by the setTimeout function.'
+        }
+      }));
+    });
+  });
 };
+
+module.exports.sorted = function(sample_points) {
+  validate(sample_points);
+
+  if( sample_points.length === 1 )
+    return () => sample_points[0];
+
+  return () => {
+    const i = Math.floor(Math.random()*(sample_points.length-1));
+    const j = i+1;
+
+    if( sample_points[i] > sample_points[j] )
+      throw new Error('Not in correct sort order: ' + sample_points);
+
+    const u = Math.random();
+    return (sample_points[i]*(1-u) + sample_points[j]*u);
+  };
+};
+
+module.exports.smart = function() {
+  if( arguments.length === 1 ) {
+    if( typeof arguments[0] === 'function' )
+      return arguments[0];
+  }
+
+  return module.exports.sorted(arguments);
+};
+
 
