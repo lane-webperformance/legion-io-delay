b0906c1d49172ac7b9fcf9dce96d889ab39debfc
Christopher Lane Hinson <lane@webperformance.com>
2017-06-07 17:34:18 -0400

    Changes as a result of discovering that the metrics merge process could saturate the event queue and interfere with our ability to collect proper timing information in a test with a lot of concurrency. The fix is in the MetricsTarget object of the legion-metrics project, but the problem was discovered in this package.

--- spec/io-delay-spec.js
+++ spec/io-delay-spec.js
@@ -6,6 +6,16 @@ const Io = require('legion-io');
 const core = require('legion-core');
 
 describe('The fetch module for legion Io', function() {
+  const SAVE_TIMEOUT = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+
+  beforeEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  });
+
+  afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = SAVE_TIMEOUT;
+  });
+
   it('is sane', function(done) {
     delay(1).run(core.Services.create().withMetricsTarget(metrics.Target.create(metrics.merge))).then(done).catch(done.fail);
   });
@@ -13,8 +23,8 @@ describe('The fetch module for legion Io', function() {
   it('measures timings correctly', function(done) {
     const target = metrics.Target.create(metrics.merge);
 
-    delay(3).run(core.Services.create().withMetricsTarget(target)).then(function() {
-      const metrics = JSON.parse(JSON.stringify(target.get()));
+    delay(3).run(core.Services.create().withMetricsTarget(target)).then(() => target.get().then(metrics => {
+      metrics = JSON.parse(JSON.stringify(metrics));
 
       expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeGreaterThan(2995);
       expect(metrics.tags.protocol['delay'].values.delay_duration.$avg.avg).toBeLessThan(3050);
@@ -22,8 +32,8 @@ describe('The fetch module for legion Io', function() {
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeGreaterThan(-5);
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeLessThan(50);
 
-      done();
-    }).catch(done.fail);
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.size).toBe(1);
+    })).then(done).catch(done.fail);
   });
 
   it('supports uniformly distributed delays', function(done) {
@@ -33,8 +43,8 @@ describe('The fetch module for legion Io', function() {
     for( let i = 0; i < 1000; i++ )
       tasks.push(delay(1,3));
 
-    Io.parallel(tasks).run(core.Services.create().withMetricsTarget(target)).then(function() {
-      const metrics = JSON.parse(JSON.stringify(target.get()));
+    Io.parallel(tasks).run(core.Services.create().withMetricsTarget(target)).then(() => target.get().then(metrics => {
+      metrics = JSON.parse(JSON.stringify(metrics));
 
       expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeGreaterThan(900);
       expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeLessThan(1100);
@@ -48,8 +58,8 @@ describe('The fetch module for legion Io', function() {
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeGreaterThan(-5);
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeLessThan(50);
 
-      done();
-    }).catch(done.fail);
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.size).toBe(1000);
+    })).then(done).catch(done.fail);
   });
 
   it('supports delays from a user-specified random number generator', function(done) {
@@ -59,8 +69,8 @@ describe('The fetch module for legion Io', function() {
     for( let i = 0; i < 1000; i++ )
       tasks.push(delay(() => Math.random() + Math.random() + 1));
 
-    Io.parallel(tasks).run(core.Services.create().withMetricsTarget(target)).then(function() {
-      const metrics = JSON.parse(JSON.stringify(target.get()));
+    Io.parallel(tasks).run(core.Services.create().withMetricsTarget(target)).then(() => target.get().then(metrics => {
+      metrics = JSON.parse(JSON.stringify(metrics));
 
       expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeGreaterThan(900);
       expect(metrics.tags.protocol['delay'].values.delay_duration.$min).toBeLessThan(1100);
@@ -74,8 +84,8 @@ describe('The fetch module for legion Io', function() {
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeGreaterThan(-5);
       expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.avg).toBeLessThan(50);
 
-      done();
-    }).catch(done.fail);
+      expect(metrics.tags.protocol['delay'].values.delay_discrepancy.$avg.size).toBe(1000);
+    })).then(done).catch(done.fail);
   });
 
   it('rejects invalid arguments', function(done) {
